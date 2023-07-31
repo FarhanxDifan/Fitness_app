40b8a18c24fe133b32150af48905cb720825935d
FarhanxDifan <abs@gmail.com>
2023-07-31 23:38:59 +0600

    first commit

---
+++ fitness_app/lib/home_page.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+
+import 'package:day34/model.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String link =
+      "https://raw.githubusercontent.com/codeifitech/fitness-app/master/exercises.json?fbclid=IwAR2gsu4SRvRRFkHK8JPTWHZXmaNP0dtpOG6h7ep4zQp7WaamX5S1UaSrc3A";
+
+  List<Exercise> allData = [];
+  late Exercise exercise;
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+  bool isLoading = false;
+  fetchData() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      var responce = await http.get(Uri.parse(link));
+      print("status code is ${responce.statusCode}");
+      if (responce.statusCode == 200) {
+        var data = jsonDecode(responce.body);
+        for (var i in data["exercises"]) {
+          exercise = Exercise(
+              id: i["id"],
+              title: i["title"],
+              thumbnail: i["thumbnail"],
+              seconds: i["seconds"]);
+          setState(() {
+            allData.add(exercise);
+          });
+        }
+        setState(() {
+          isLoading = false;
+        });
+      }
+      setState(() {
+        isLoading = false;
+      });
+      //
+
+    } catch (maria) {
+      setState(() {
+        isLoading = false;
+      });
+      print("the problem is $maria");
+      showToast("Somthing wrong bro");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: isLoading == true,
+      blur: 0.5,
+      opacity: 0.5,
+      progressIndicator: CircularProgressIndicator(),
+      child: Scaffold(
+        body: ListView.builder(
+            shrinkWrap: true,
+            itemCount: allData.length,
+            itemBuilder: (context, index) {
+              return Text("${allData[index].title}");
+            }),
+      ),
+    );
+  }
+}
+
+showToast(String title) {
+  return Fluttertoast.showToast(
+      msg: "$title",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
